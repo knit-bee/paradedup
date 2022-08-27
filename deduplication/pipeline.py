c2573356301a8f9c49249d83ec787c0da10cfb94
luise koehler <luise.sj.koehler@gmail.com>
2022-08-27 14:02:59 +0200

    Implement ndd pipeline draft

---
+++ deduplication/pipeline.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass, field
+from typing import Dict
+
+from deduplication.candidate_ranker import CandidateRanker
+from deduplication.candidate_searcher import CandidateSearcher
+from deduplication.document_processing import DocumentProcessor
+from deduplication.minhashing import MinHasher, DocumentSketch
+from deduplication.preprocessor import Preprocessor
+
+
+@dataclass
+class NddPipeline:
+    document_processor: DocumentProcessor
+    preprocessor: Preprocessor
+    minhasher: MinHasher
+    candidate_searcher: CandidateSearcher
+    candidate_ranker: CandidateRanker
+    id_to_doc_sketch_mapping: Dict[str, DocumentSketch] = field(default_factory=dict)
+
+    def find_near_duplicates(self, directory: str) -> Dict[str, list]:
+        for file_name, content in self.document_processor.load_files(directory):
+            self._add_file_to_database(file_name, content)
+        nd_candidates = {}
+        for file_id, doc_sketch in self.id_to_doc_sketch_mapping.items():
+            candidates = self.candidate_searcher.get_candidates_for_document(doc_sketch)
+            nd_candidates[doc_sketch.file_name] = [
+                self.id_to_doc_sketch_mapping[candidate].file_name
+                for candidate in candidates
+            ]
+        return nd_candidates
+
+    def _add_file_to_database(self, file_name: str, file_content: str) -> None:
+        shingles = self.preprocessor.preprocess_document(file_content)
+        doc_sketch = self.minhasher.create_document_sketch(file_name, shingles)
+        self.id_to_doc_sketch_mapping[doc_sketch.doc_id] = doc_sketch
+        self.candidate_searcher.insert_document_sketch(doc_sketch)
---
+++ tests/test_pipeline.py
@@ -0,0 +1,42 @@
+import unittest
+from typing import Generator
+
+from deduplication.candidate_ranker import CandidateRanker
+from deduplication.candidate_searcher import CandidateSearcher
+from deduplication.minhashing import MinHasher
+from deduplication.pipeline import NddPipeline
+from deduplication.preprocessor import Preprocessor
+
+
+class MockDocumentProcessor:
+    test_data = ["Lorem ipsum dolor sit amet, consectetur adipisici elit, sed"
+    " eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim"
+    " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.",
+     " eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim"
+     "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed"
+     " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.",
+     "ein anderer Text, der ganz anders ist und nicht als Duplikat gefunden werden sollte"
+      ]
+    def load_files(self, directory: str) -> Generator[str, None, None]:
+        for i, doc in enumerate(self.test_data):
+            yield f"file{i}", doc
+
+
+class NddPipelineTester(unittest.TestCase):
+    def setUp(self):
+        self.doc_processor = MockDocumentProcessor()
+        self.preprocessor = Preprocessor()
+        self.minhasher = MinHasher()
+        self.searcher = CandidateSearcher()
+        self.ranker = CandidateRanker()
+        self.pipeline = NddPipeline(
+            document_processor=self.doc_processor,
+            preprocessor=self.preprocessor,
+            minhasher=self.minhasher,
+            candidate_searcher=self.searcher,
+            candidate_ranker=self.ranker,
+        )
+
+    def test_run_pipeline(self):
+        result = self.pipeline.find_near_duplicates('mock_dir')
+        self.assertEqual(result, {'file0':['file1'], 'file1':['file0'], 'file2':[]})
