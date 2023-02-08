abd714ffea001e4615432aee6b18979f9871a175
luise koehler <luise.sj.koehler@gmail.com>
2023-02-08 18:12:07 +0100

    Change processing method of files and output

--- deduplication/pipeline.py
+++ deduplication/pipeline.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Dict
+from typing import Dict, List, Tuple
 from uuid import UUID
 
 from deduplication.candidate_ranker import CandidateRanker
@@ -18,15 +18,23 @@ class NddPipeline:
     candidate_ranker: CandidateRanker
     id_to_doc_sketch_mapping: Dict[UUID, DocumentSketch] = field(default_factory=dict)
 
-    def find_near_duplicates(self, directory: str) -> Dict[str, list]:
+    def process_files(self, directory: str) -> None:
         for file_name, content in self.document_processor.load_files(directory):
             self._add_file_to_database(file_name, content)
+
+    def find_near_duplicates(self) -> Dict[str, List[Tuple[str, float]]]:
         nd_candidates = {}
         for file_id, doc_sketch in self.id_to_doc_sketch_mapping.items():
-            candidates = self.candidate_searcher.get_candidates_for_document(doc_sketch)
+            candidates = [
+                self.id_to_doc_sketch_mapping[candidate]
+                for candidate in self.candidate_searcher.get_candidates_for_document(
+                    doc_sketch
+                )
+            ]
+            ranked = self.candidate_ranker.rank_candidates(doc_sketch, candidates)
             nd_candidates[doc_sketch.file_name] = [
-                self.id_to_doc_sketch_mapping[candidate].file_name
-                for candidate in candidates
+                (self.id_to_doc_sketch_mapping[candidate].file_name, sim_score)
+                for candidate, sim_score in ranked
             ]
         return nd_candidates
 
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,21 +1,29 @@
 import unittest
 from typing import Generator, Tuple
+from uuid import UUID
 
 from deduplication.candidate_ranker import CandidateRanker
 from deduplication.candidate_searcher import CandidateSearcher
-from deduplication.minhashing import MinHasher
+from deduplication.minhashing import DocumentSketch, MinHasher
 from deduplication.pipeline import NddPipeline
 from deduplication.preprocessor import Preprocessor
 
 
 class MockDocumentProcessor:
     test_data = [
+        # file0
         "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed"
         " eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim"
         " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.",
+        # file1
+        " eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim"
+        "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed"
+        " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequät.",
+        # file2
         " eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim"
         "Lorem ipsum dolor sit amet, consectetur adipisici elit, sed"
         " veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.",
+        # file3
         "ein anderer Text, der ganz anders ist und nicht als Duplikat gefunden werden sollte",
     ]
 
@@ -39,6 +47,38 @@ class NddPipelineTester(unittest.TestCase):
             candidate_ranker=self.ranker,
         )
 
-    def test_run_pipeline(self):
-        result = self.pipeline.find_near_duplicates("mock_dir")
-        self.assertEqual(result, {"file0": ["file1"], "file1": ["file0"], "file2": []})
+    def test_process_files(self):
+        self.pipeline.process_files("mock_dir")
+        self.assertTrue(
+            isinstance(list(self.pipeline.id_to_doc_sketch_mapping.keys())[0], UUID)
+        )
+        self.assertTrue(
+            isinstance(
+                list(self.pipeline.id_to_doc_sketch_mapping.values())[0], DocumentSketch
+            )
+        )
+
+    def test_candidates_found(self):
+        self.pipeline.process_files("mock_dir")
+        ranked = self.pipeline.find_near_duplicates()
+        result = {
+            key: [filename for filename, score in val] for key, val in ranked.items()
+        }
+        self.assertEqual(
+            result,
+            {
+                "file0": ["file2", "file1"],
+                "file1": ["file2", "file0"],
+                "file2": ["file1", "file0"],
+                "file3": [],
+            },
+        )
+
+    def test_candidates_ranked(self):
+        self.pipeline.process_files("mock_dir")
+        ranked = self.pipeline.find_near_duplicates()
+        for file, results in ranked.items():
+            with self.subTest():
+                self.assertEqual(
+                    sorted(results, key=lambda x: x[1], reverse=True), results
+                )
