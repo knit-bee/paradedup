2e91f6078d5bbe6131b6b442f84b090b4a4b73fc
luise koehler <luise.sj.koehler@gmail.com>
2023-02-26 20:22:53 +0100

    Implement use case

--- deduplication/cli/use_case.py
+++ deduplication/cli/use_case.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 from typing import Protocol
-
+import json
+from deduplication.candidate_ranker import CandidateRanker
+from deduplication.candidate_searcher import CandidateSearcher
+from deduplication.document_processing import DocumentProcessorImpl
+from deduplication.minhashing import MinHasher
 from deduplication.pipeline import NddPipeline
+from deduplication.preprocessor import Preprocessor
 
 
 @dataclass
@@ -27,4 +32,24 @@ class DeduplicationUseCaseImpl:
     """Use case for deduplication pipeline call from command line"""
 
     def process(self, request: Request) -> None:
-        pass
+        preprocessor = Preprocessor(
+            shingle_size=request.shingle_size,
+            case_insensitive=request.case_insensitive,
+            ignore_nums=request.ignore_nums,
+            normalize_whitespace=request.normalize_whitespace,
+            ignore_interpunctuation=request.ignore_interpunctuation,
+            use_token=request.use_token,
+        )
+        minhasher = MinHasher(num_perm=request.num_perm)
+        doc_processor = DocumentProcessorImpl()
+        candidate_searcher = CandidateSearcher(
+            lsh_threshold=request.threshold, num_perm=request.num_perm
+        )
+        candidate_ranker = CandidateRanker()
+        pipeline = NddPipeline(
+            doc_processor, preprocessor, minhasher, candidate_searcher, candidate_ranker
+        )
+        pipeline.process_files(request.directory)
+        nd_candidates = pipeline.find_near_duplicates()
+        with open(request.output_file, "w") as fh:
+            json.dump(nd_candidates, fh)
